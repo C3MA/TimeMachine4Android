d0a4e1633497ac7701c32378343016f81fb1fc39
Ollo <none@none>
2012-08-19 14:42:33 +0200

    also display all logs

--- src/de/c3ma/timemachine4android/LastActionListActivity.java
+++ src/de/c3ma/timemachine4android/LastActionListActivity.java
@@ -1,9 +1,13 @@
 package de.c3ma.timemachine4android;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import de.c3ma.timemachine4android.list.MessageListCursorAdapter;
 import de.c3ma.timemachine4android.persitance.LoggerHelper;
 
@@ -24,5 +28,25 @@ public class LastActionListActivity extends ListActivity implements Constants {
         Cursor cur = LoggerHelper.getCursorLastMessages(db);
         setListAdapter(new MessageListCursorAdapter(this, R.layout.message_item, cur));
         db.close();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.lastaction, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.show_all_logs:
+                Intent intent = new Intent(this, AllLogsListActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
